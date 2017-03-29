10820e42112ac60d0e43b5132b97f418c2bf8957
Piotr Hechelski <piotrek.h@hotmail.com>
2017-03-29 20:06:41 +0200

    created initial project structure; added asp.net identity sql scripts

---
+++ PlanningPoker.Database/Program.cs
@@ -0,0 +1,54 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace PlanningPoker.Database
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            string connectionString = args.FirstOrDefault() ?? config["connectionString"];
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsFromFileSystem("Scripts")
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+
+#if DEBUG
+            Console.ReadLine();
+#endif
+
+            return 0;
+        }
+    }
+}
